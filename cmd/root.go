04f63ce9f691c0e0d20120934dc0118c147820f7
Julien SENON <julien.senon@gmail.com>
2018-07-14 00:06:34 +0200

    Fix copyright

--- cmd/root.go
+++ cmd/root.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 Julein SENON <julien.senon@gmail.com>
+// Copyright © 2018 Julien SENON <julien.senon@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
